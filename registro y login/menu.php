b2d4a89595e98101625f8846952d3a72ed1c72c2
juancarente28 <115377405+juancarente28@users.noreply.github.com>
2024-02-25 21:03:11 -0500

    Add files via upload
    trabajo sena alides y juan

---
+++ registro y login/menu.php
@@ -0,0 +1,30 @@
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bien venido</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+<div class="page-header">
+    <h1>Hola <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b> Bienvenido.</h1>
+</div>
+<p>
+    <a href="salir.php" class="btn btn-danger">cerrar sesión</a>
+</p>
+</body>
+</html>
